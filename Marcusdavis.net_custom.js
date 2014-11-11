d14fe0143a20c8feb7caae3b883fa8757dc5c73b
Marcusjamaal01 <marcus.davis3@gmail.com>
2014-11-11 17:29:18 -0500

    Create Marcusdavis.net_custom.js
    A JavaScript function created for a CSS file in my IT Portfolio site.

---
+++ Marcusdavis.net_custom.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+ function my_kwicks(){
+    $('.kwicks').kwicks({
+        duration: 300,
+        max: 200,
+        spacing:0
+		
+    });
+}
+
+ $(document).ready(function(){
+	my_kwicks();
+});
+
+
